61effce03007b0fae9f7d9af284a4e3fce69eb83
Aseem rai <raiaseem25@gmail.com>
2023-09-04 17:46:59 +0545

    feat:add .

---
+++ Vrit/Day 11 Assignments/Day 13/scripts/index.js
@@ -0,0 +1,83 @@
+console.log('I am here')
+
+// Syntax, Declaring variables, Data Types
+// => var, let, const
+
+// Data Types
+
+// Number 1, 2, 3, 4,5
+let num = 2;
+const secNum = 10;
+console.log(num, typeof num) 
+
+// String
+let str = 'Hi my name is Aeron'
+const str1 ='2';
+console.log(str, typeof str)
+console.log(str1, typeof str1)
+
+// Boolean
+let bool1 =true;
+let bool2 =false;
+
+console.log(bool1, typeof bool1)
+console.log(bool2, typeof bool2)
+
+// Undefined
+
+let hello;
+console.log(typeof hello)
+
+// Null
+let abc =  null;
+console.log(typeof abc)
+
+// BigInt => Large numbers
+
+// Symbol
+
+// Object
+// key: value pair
+let student1 = {
+    name:'Prashant',
+    age:28,
+    rollNo:12,
+    isPresent:false
+}
+console.log( student1, typeof student1)
+console.log(student1.name, typeof student1.name)
+console.log(student1.age, typeof student1.age)
+
+// Array
+let Arr = [1, 2, 3, 4, 5]
+console.log(Arr)
+
+let students = ['Ram', 'Hari', 'Sita']
+console.log(students)
+
+// Operators
+// 1. Arithmetic Operators
+
+const num1 = 10;
+const num2 = 20;
+const sum = num1 + num2;
+console.log(sum)
+
+const sub = num1 - num2;
+console.log(sub)
+
+const mul = num1 * num2;
+console.log(mul)
+
+const div = num1 / num2;
+console.log(div)
+
+const rem = num1 % num2;
+console.log(rem)
+
+
+
+
+
+
+
